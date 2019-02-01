6b57a334564399686a8fed4646ec2a3744cf26b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-01 10:59:49 +0000

    Deploy liao961120/parallelCode to github.com/liao961120/parallelCode.git:gh-pages

--- addons/pre_chap.R
+++ addons/pre_chap.R
@@ -5,3 +5,14 @@ gh_edit_path <- paste0('https://github.com/liao961120/parallelCode/edit/master/'
 edit_btn <- paste0('<a href="', gh_edit_path, '">',
                    '<i class="fa fa-edit" ',
                    'style="font-size: 0.5em;"></i></a>')
+
+# Set knitr options
+options(knitr.duplicate.label = 'allow')
+knitr::opts_chunk$set(
+	echo = TRUE,
+	message = FALSE,
+	warning = FALSE,
+	dev='svg',
+	comment = '#>',
+	fig.dim = c(5.5, 3.5)   # width height
+	)
