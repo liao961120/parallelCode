6eac96dd40306e233cefed56617688072d627086
liao <liaomovie2@gmail.com>
2019-02-10 13:11:01 +0800

    remove fontawsome dependencies

--- addons/pre_chap.R
+++ addons/pre_chap.R
@@ -3,8 +3,12 @@ reticulate::use_python(Sys.which("python3"), required = T)
 gh_edit_path <- paste0('https://github.com/liao961120/parallelCode/edit/master/', knitr::current_input())
 
 edit_btn <- paste0('<a href="', gh_edit_path, '">',
-                   '<i class="fa fa-edit" ',
-                   'style="font-size: 0.5em;"></i></a>')
+                   '<img src="https://bit.ly/2RRirG7" ',
+                   'alt="fa-edit" ',
+                   'class="edit"></a>')
+
+# https://bit.ly/2RRirG7
+# source: https://upload.wikimedia.org/wikipedia/commons/4/4c/Edit_font_awesome.svg
 
 # Set knitr options
 options(knitr.duplicate.label = 'allow')
--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ Rscript -e 'bookdown::render_book("index.Rmd")'
 echo 'Moving output files to `docs/` ...'
 mv out.html _bookdown_files/index.html
 [[ -e out.rds ]] && mv out.rds _bookdown_files/
-cp -r addons _bookdown_files
+cp -r addons/ _bookdown_files/
 
 [[ -d docs ]] && rm -r docs
 mv _bookdown_files docs
